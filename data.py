eb3278c62c19499626c02aa4cec66eb5ab04eaa7
Leonardo Daher <leonardo.daher@usp.br>
2017-06-07 00:47:14 -0300

    Get data from Mercado Bitcoin

---
+++ data.py
@@ -0,0 +1,37 @@
+"Script to import data from Mercado Bitcoin API"
+
+import requests
+from datetime import datetime
+from pytz import utc
+from pymongo import MongoClient
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+client = MongoClient()
+database = client['mercado_bitcoin']
+collection = database['data']
+
+def tick():
+    "Import data from Mercado Bitcoin and throw it in a MongoDB collection"
+
+    ticker = requests.get("https://www.mercadobitcoin.net/api/ticker/").json()
+    depth = requests.get("https://www.mercadobitcoin.net/api/orderbook/").json()
+
+    date = datetime.fromtimestamp(int(ticker['ticker']['date']))
+    price = float(ticker['ticker']['last'])
+    v_bid = sum([bid[1] for bid in depth['bids']])
+    v_ask = sum([ask[1] for ask in depth['asks']])
+
+    collection.insert({'date':date, 'price':price, 'v_bid':v_bid, 'v_ask':v_ask})
+    print(date, price, v_bid, v_ask)
+
+def main():
+    "Run tick() every 10 seconds"
+
+    scheduler = BlockingScheduler(timezone=utc)
+    scheduler.add_job(tick, 'interval', seconds=10)
+    try:
+        scheduler.start()
+    except(KeyboardInterrupt, SystemExit):
+        pass
+
+main()
